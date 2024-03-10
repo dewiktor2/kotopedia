c9ce8af941292575c3a3d7a868a5fe9e6347575b
Wiktor Dębski <dewiktor@gmail.com>
2024-03-10 17:13:39 +0100

    poprawki

--- apps/baza-karm/src/app/utility/components/search-input.component.ts
+++ apps/baza-karm/src/app/utility/components/search-input.component.ts
@@ -9,15 +9,15 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="form-control pb-4 flex w-100">
-      <div class="place-items-center min-w-80">
-        <label class="input input-bordered flex w-100 items-center gap-2">
+    <div class="form-control pb-4 flex w-72">
+      <div class="place-items-center flex">
+        <label class="input input-bordered flex w-72 items-center gap-2">
           <input
-            [readonly]="(searchInProgress$ | async)"
+            [readonly]="searchInProgress$ | async"
             #searchInput
             type="search"
             class="grow"
-            placeholder="Szukaj po firmach i nazwie"
+            placeholder="Szukaj"
             (keydown.enter)="onSearchButtonClick(searchInput.value)"
           />
           <svg
@@ -34,6 +34,26 @@ import { CommonModule } from '@angular/common';
             />
           </svg>
         </label>
+        <!-- Information Icon with Tooltip -->
+        <div
+          class="flex items-center ml-2 tooltip"
+          data-tip="Szukaj po nazwie lub firmie"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 cursor-pointer text-blue-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   `,
@@ -44,13 +64,6 @@ import { CommonModule } from '@angular/common';
         display: flex;
         flex-direction: row;
         gap: 8px;
-      }
-      input {
-        width: 100%; /* Makes the input field take up all available space */
-        max-width: 400px; /* Optionally, set a maximum width to prevent it from getting too wide */
-      }
-      .tooltip {
-        display: block;
       }
     `,
   ],
@@ -65,10 +78,12 @@ export class SearchInputComponent {
 
   ngOnInit() {
     this.searchInProgress$ = this.store.select(FeedsState.searchInProgress);
-    this.searchSubject.pipe(
-      debounceTime(500), // Wait for 500ms of silence before emitting the last value
-      distinctUntilChanged() // Only emit if the current value is different from the last
-    ).subscribe(value => this.searchText.emit(value));
+    this.searchSubject
+      .pipe(
+        debounceTime(500), // Wait for 500ms of silence before emitting the last value
+        distinctUntilChanged() // Only emit if the current value is different from the last
+      )
+      .subscribe((value) => this.searchText.emit(value));
   }
 
   ngOnDestroy() {

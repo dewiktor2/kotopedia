31e10e56c233f7ce8b715a944254271afaf61036
Wiktor Dębski <dewiktor@gmail.com>
2025-01-20 21:30:27 +0100

    rm ignore

--- apps/baza-karm/src/env/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://wbxehfjwhndemwklafre.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndieGVoZmp3aG5kZW13a2xhZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MjcyMjYsImV4cCI6MjAyMjEwMzIyNn0.jo3siU-YEcYVjhrfK0syJfIAe8pHwyRA2ydb_NODmO0',
-};

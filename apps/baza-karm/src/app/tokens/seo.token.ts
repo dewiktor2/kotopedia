22e7203a69cf2c5499c86752169f900aba317a32
Wiktor Dębski <dewiktor@gmail.com>
2025-01-23 00:22:20 +0100

    seo canonical

--- apps/baza-karm/src/app/tokens/seo.token.ts
+++ apps/baza-karm/src/app/tokens/seo.token.ts
@@ -1,6 +1,6 @@
-
 import { DestroyRef, inject, InjectionToken } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DOCUMENT } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs/operators';
@@ -13,9 +13,22 @@ export const SEO_HANDLER = new InjectionToken<void>('SEO_HANDLER', {
     const router = inject(Router);
     const activatedRoute = inject(ActivatedRoute);
     const destroyRef = inject(DestroyRef);
+    const document = inject(DOCUMENT);
 
     const defaultDescription = 'Znajdź najlepsze karmy dla swojego kota.';
     const defaultKeywords = 'kot, karma dla kota, karmy dla kotów, zdrowie kota';
+
+    const updateCanonicalUrl = (url: string) => {
+      let link: HTMLLinkElement | null = document.querySelector('link[rel="canonical"]');
+
+      if (!link) {
+        link = document.createElement('link');
+        link.setAttribute('rel', 'canonical');
+        document.head.appendChild(link);
+      }
+
+      link.setAttribute('href', url);
+    };
 
     return () => router.events
       .pipe(
@@ -28,18 +41,18 @@ export const SEO_HANDLER = new InjectionToken<void>('SEO_HANDLER', {
           return route;
         }),
         takeUntilDestroyed(destroyRef),
-        switchMap((route) => route.data)
+        switchMap((route) => route.data || {})
       )
       .subscribe((data) => {
         const title = data['title'] || 'Kotopedia - baza karm dla kotów';
         const description = data['description'] || defaultDescription;
         const keywords = data['keywords'] || defaultKeywords;
-        // const canonicalUrl = `${window.location.origin}${router.url}`;
+        const canonicalUrl = `${document.location.origin}${router.url}`;
 
         titleService.setTitle(title);
         meta.updateTag({ name: 'description', content: description });
         meta.updateTag({ name: 'keywords', content: keywords });
-        // meta.updateTag({ name: 'canonical', content: canonicalUrl });
+        updateCanonicalUrl(canonicalUrl);
       });
   },
 });

9ac5276cab8b36b5a0bed38d8ae9cef024e0a500
Wiktor Dębski <dewiktor@gmail.com>
2025-02-16 20:37:55 +0100

    chore

--- apps/baza-karm/src/fakeEnv/fakeForCI.ts
+++ apps/baza-karm/src/fakeEnv/fakeForCI.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    production: true,
-    supabaseUrl: '',
-    supabaseKey: '',
-    hCaptchaSecret: ''
-  };
-  
+  production: true,
+  supabaseUrl: '',
+  supabaseKey: '',
+  hCaptchaSecret: '',
+};
